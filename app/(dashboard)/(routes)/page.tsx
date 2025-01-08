79a08adcbf843c6b19084ba0ec1334d11db73826
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-09 00:32:09 +0530

    added navbar routes

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <div>
-      <UserButton /> 
+      <div className="text-3xl">Home</div>
     </div>
   );
 }
--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -4,11 +4,11 @@ import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
-interface SidebarItemProps {
+type SidebarItemProps = {
   icon: LucideIcon;
   label: string;
   href: string;
-}
+};
 export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
