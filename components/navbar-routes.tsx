3b176b8df61c8c6212145a74142baca61479f12f
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-09 22:04:34 +0530

    added teacher routes and pages

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,34 @@
-"use-client";
+"use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
-export const NavbarRoutes = () => {
+export const NavbarRoutes = () => { 
+  const pathname = usePathname();
+
+  const isTeacher = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
+      {isTeacher || isPlayerPage ? (
+        <Link href="/">
+          <Button size="lg" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="lg" variant="ghost">
+            Teacher Mode
+          </Button>
+        </Link>
+      )}
       <UserButton />
     </div>
   );
