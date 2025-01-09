76539876ad6363d7b51034489e68fe1d4b931917
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-10 01:11:19 +0530

    added create course page and form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import ToastProvider from "@/components/providers/toaster-provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "LMS Project",
@@ -14,7 +18,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
