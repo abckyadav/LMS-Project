67993013a401fa5374ba9dbaa6eed87d448fd826
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-13 02:44:23 +0530

    create chapter form component and chapter list to show chapters and reordering

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -17,9 +17,10 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import toast from "react-hot-toast";
-import { PlusCircle } from "lucide-react";
+import { Loader2, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import ChaptersList from "./chapters-list";
 
 type ChaptersFormProps = {
   initialData: Course & { chapters: Chapter[] };
@@ -61,8 +62,34 @@ export default function ChaptersForm({
       toast.error("Something went wrong while updating course");
     }
   };
+
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+
+      toast.success("Chapters reordered successfully");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong while reordering chapters");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-cneter justify-between">
         Course chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -113,9 +140,13 @@ export default function ChaptersForm({
             !initialData.chapters.length && "text-slate-500 italic"
           )}
         >
-          {!initialData.chapters.length
-            ? "No chapters"
-            : initialData.chapters.map((chapter) => <div>{chapter.title}</div>)}
+          {!initialData.chapters.length && "No chapters yet !"}
+
+          <ChaptersList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
         </div>
       ) : null}
       {!isCreating ? (
