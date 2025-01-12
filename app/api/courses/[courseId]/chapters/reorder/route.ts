04f7c2cd513e6495185941148aa67d3d0f16c355
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-13 02:43:02 +0530

    create api for reorder chapters

---
+++ app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -0,0 +1,47 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const { list } = await req.json();
+
+    const ownCourse = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!ownCourse) {
+      return new NextResponse("Forbidden", { status: 401 });
+    }
+
+    for (const item of list) {
+      await db.chapter.update({
+        where: {
+          id: item.id,
+        },
+        data: {
+          position: item.position,
+        },
+      });
+    }
+
+    return new NextResponse("Course chapters reordered successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("[REORDER]", error);
+    return new NextResponse("Internal error while reordering", { status: 500 });
+  }
+}
