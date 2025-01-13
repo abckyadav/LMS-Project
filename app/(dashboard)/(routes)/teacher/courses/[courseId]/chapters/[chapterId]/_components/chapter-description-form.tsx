8d2f78916683db8885692848b321c4a7a641e62e
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-13 12:44:34 +0530

    added react-quill for rich text editor

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-description-form.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { useState } from "react";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { Chapter } from "@prisma/client";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import toast from "react-hot-toast";
+import { Pencil } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Editor from "@/components/editor";
+import Preview from "@/components/preview";
+
+type ChapterDescriptionFormProps = {
+  initialData: Chapter;
+  courseId: string;
+  chapterId: string;
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+const formSchema = z.object({
+  description: z.string().min(2),
+});
+
+export default function ChapterDescriptionForm({
+  initialData,
+  courseId,
+  chapterId,
+}: ChapterDescriptionFormProps) {
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const toggleEdit = () => setIsEditing((currentValue) => !currentValue);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      description: initialData?.description || "",
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      await axios.patch(
+        `/api/courses/${courseId}/chapters/${chapterId}`,
+        values
+      );
+      toast.success("Chapter updated successfully");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong while updating course");
+    }
+  };
+  return (
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-cneter justify-between">
+        Chapter description
+        <Button onClick={toggleEdit} variant="ghost">
+          {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit description
+            </>
+          )}
+        </Button>
+      </div>
+      {!isEditing ? (
+        <div
+          className={cn(
+            "text-sm mt-2",
+            !initialData.description && "text-slate-500 italic"
+          )}
+        >
+          {!initialData.description && "No description"}
+          {initialData.description && (
+            <Preview value={initialData.description} />
+          )}
+        </div>
+      ) : null}
+
+      {isEditing ? (
+        <Form {...form}>
+          <form
+            className="space-y-4 mt-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Editor {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="flex items-center gap-x-2">
+              <Button disabled={!isValid || isSubmitting}>Save</Button>
+            </div>
+          </form>
+        </Form>
+      ) : null}
+    </div>
+  );
+}
