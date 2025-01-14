eb4e3c776ec9a111d5f4fffb9c49401c23923859
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-14 16:19:13 +0530

    add search input component and integrate into search page and navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -6,30 +6,39 @@ import React from "react";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
+import SearchInput from "./search-input";
 
-export const NavbarRoutes = () => { 
+export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isTeacher = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
-    <div className="flex gap-x-2 ml-auto">
-      {isTeacher || isPlayerPage ? (
-        <Link href="/">
-          <Button size="lg" variant="ghost">
-            <LogOut className="h-4 w-4 mr-2" />
-            Exit
-          </Button>
-        </Link>
-      ) : (
-        <Link href="/teacher/courses">
-          <Button size="lg" variant="ghost">
-            Teacher Mode
-          </Button>
-        </Link>
-      )}
-      <UserButton />
-    </div>
+    <>
+      {isSearchPage ? (
+        <div className="hidden md:block">
+          <SearchInput />
+        </div>
+      ) : null}
+      <div className="flex gap-x-2 ml-auto">
+        {isTeacher || isPlayerPage ? (
+          <Link href="/">
+            <Button size="lg" variant="ghost">
+              <LogOut className="h-4 w-4 mr-2" />
+              Exit
+            </Button>
+          </Link>
+        ) : (
+          <Link href="/teacher/courses">
+            <Button size="lg" variant="ghost">
+              Teacher Mode
+            </Button>
+          </Link>
+        )}
+        <UserButton />
+      </div>
+    </>
   );
 };
