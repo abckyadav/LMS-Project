da09d52d86c06d965c07bd737cbc43ab5d1f186e
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-14 15:39:57 +0530

    add category display components and integrate with search page

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,17 @@
-const SearchPage = () => {
-  return <div className="text-3xl">This is a Search Page</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;
